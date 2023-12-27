fd6d8c3339a2b104bc4ea232727d820123def77b
Joseph De Leon <joeseffdl@gmail.com>
2023-12-27 14:10:54 +0800

    Initialization

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next"
+import { Montserrat } from "next/font/google"
+import "./globals.css"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  fallback: [
+    "ui-sans-serif",
+    "system-ui",
+    "sans-serif",
+    "Apple Color Emoji",
+    "Segoe UI Emoji",
+    "Segoe UI Symbol",
+    "Noto Color Emoji",
+  ],
+})
+
+export const metadata: Metadata = {
+  title: "Joseph De Leon | Web Developer",
+  description: "Web developer based in the Philippines.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        {/* <NavigationBar /> */}
+        {children}
+        {/* <Footer /> */}
+      </body>
+    </html>
+  )
+}
