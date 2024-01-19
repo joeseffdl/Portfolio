057d43a9627f9b8581c09e29a0812ae371f75810
Joseph De Leon <joeseffdl@gmail.com>
2024-01-19 16:19:24 +0800

    Updated About page

--- app/[linkedPages]/_LinkedPages/About.tsx
+++ app/[linkedPages]/_LinkedPages/About.tsx
@@ -11,13 +11,14 @@ export const AboutPage = () => (
           👋 You can call me <span className="text-sky-400">Zeph</span>
         </h1>
         <p className="text-lg sm:px-5 duration-150">
-          Welcome to my world of 🎸 <span className="text-sky-400">Music</span>{" "}
-          and 🕸️ <span className="text-sky-400">Web development</span>. Let's
-          create something amazing together! 🚀
+          Welcome to my world of 🎸 <span className="text-sky-400">Music</span>,
+          🚲 <span className="text-sky-400">Cycling</span>, and 🕸️{" "}
+          <span className="text-sky-400">Web development</span>. Let's create
+          something amazing together! 🚀
         </p>
       </div>
     </section>
-    <section className="flex flex-col gap-5 min-h-[500px] py-20 lg:px-32">
+    <section className="flex flex-col gap-5 min-h-[300px] py-20 lg:px-32">
       <h1 className="text-center">Who is Zeph?</h1>
       <p className="text-lg font-medium">
         A <span className="text-sky-700">Computer Engineering</span> graduate
@@ -36,12 +37,56 @@ export const AboutPage = () => (
           className="text-sky-700 underline"
         >
           Stardust and Melodies
-        </Link>{" "}
+        </Link>
+        . Aside from music, I also enjoy cycling and exploring the world while
+        seeking inspirations and ideas along my journey.
         <br />
         <br />
         Currently, I am actively seeking opportunities as a{" "}
         <span className="text-sky-700">Full Stack Web Developer</span>.
       </p>
     </section>
+    <section className="flex flex-col gap-5 min-h-[300px] py-20 lg:px-32">
+      <h1 className="text-center">Web Developer</h1>
+      <p className="text-lg font-medium">
+        As a passionate <span className="text-sky-700">Web Developer</span>, I
+        thrive on turning ideas into digital reality. With a strong foundation
+        in computer engineering and a commitment to staying updated with the
+        latest technologies, I specialize in creating robust and user-friendly
+        web applications. My skills range from front-end technologies like React
+        and Next.js to back-end technologies like Node.js and MongoDB. Let's
+        build innovative and impactful solutions together!
+      </p>
+    </section>
+
+    <section className="flex flex-col gap-5 min-h-[300px] py-20 lg:px-32">
+      <h1 className="text-center">Musician</h1>
+      <p className="text-lg font-medium">
+        🎸 Music is not just a hobby; it's a way of life for me. As an avid{" "}
+        <span className="text-sky-700">Musician</span>, I delve into diverse
+        genres such as rock, blues, and jazz. Whether solo or with my band,{" "}
+        <Link
+          href="https://www.facebook.com/stardustandmelodies"
+          className="text-sky-700 underline"
+        >
+          Stardust and Melodies
+        </Link>
+        , I express myself through the electric guitar. The creative process of
+        composing, performing, and connecting with the audience brings me
+        immense joy. Let the melodies resonate and inspire!
+      </p>
+    </section>
+
+    <section className="flex flex-col gap-5 min-h-[300px] py-20 mb-20 lg:px-32">
+      <h1 className="text-center">Cyclist</h1>
+      <p className="text-lg font-medium">
+        🚲 As a dedicated <span className="text-sky-700">Cyclist</span>, I
+        embrace the open road as my canvas for exploration. Cycling is not just
+        a physical activity but a journey of self-discovery and inspiration.
+        Riding through scenic routes, I find solace and recharge my creative
+        energies. Join me on this adventure as we pedal through life, uncovering
+        new perspectives and enjoying the simple pleasures along the way.
+      </p>
+    </section>
   </main>
-);
+)
